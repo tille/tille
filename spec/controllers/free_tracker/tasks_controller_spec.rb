7d4cc5632f72f53c1a450f04eb2f7236619df3ff
Jorman Bustos <tille100@gmail.com>
2014-03-31 01:38:54 -0500

    includes ControllerMacros in spec_helper.rb

--- spec/controllers/free_tracker/tasks_controller_spec.rb
+++ spec/controllers/free_tracker/tasks_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe FreeTracker::TasksController do
+  login_user
 
   describe "GET 'index'" do
     it "returns http success" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -35,4 +34,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # includes ControllerMacros to serve macros on specs
+  config.extend ControllerMacros, :type => :controller
 end
