b5ee71004aa43db1ac60fc66e5d456f9f4bb3cc4
Jorman Bustos <tille100@gmail.com>
2014-03-31 03:19:18 -0500

    fixes active nav bar selector

--- app/assets/javascripts/header_actions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-  $(".nav-tabs li").click(function(e){
-    // e.preventDefault();
-    $(".nav-tabs li").removeClass("active");
-    $(this).addClass("active");
-  });
-});
